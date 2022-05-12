c282ff32ec57544748304e09f057425de055624a
Derek <derek.q.jackson@gmail.com>
2022-05-11 22:23:59 -0700

    completed ex13 extra credit

--- ex13.c
+++ ex13.c
@@ -2,12 +2,12 @@
 
 int main (int argc, char *argv[]) {
 
-	int i = 0;
+	// commented because you can declare variables in for loop initializer...int i = 0;
 
 	// go through each string in argv
 	// why am I skipping argv[0] -> because argv[0] is actually the program call of ./ex13
-
-	for (i = 1; i < argc; i++) {
+	// if you set i = 0 below, it still works but it will print argv[0]
+	for (int i = 1; i < argc; i++) {
 
 		printf("arg %d: %s\n", i, argv[i]);
 	}
@@ -21,11 +21,16 @@ int main (int argc, char *argv[]) {
 		"Texas"
 	};
 
+	// set num_states to a higher value and see what it does
+	// set to 5 works fine, just prints the null \0 that ends all strings
+	// higher than 5 results in segmentation fault
 	int num_states = 4;
 
-	for (i = 0; i< num_states; i++) {
+	for (int i = 0; i < num_states; i++) {
 		printf("state %d: %s\n", i, states[i]);
 	}
+
+	// NULL is a pointer while '\0' is a null character object
 
 	return 0;
 }
